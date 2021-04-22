77ae9d8a681c3b19092677b04a4bf86a90d996e7
Alain Poirier <alain.abc@gmail.com>
2021-04-21 21:14:37 -0400

    Complete Final Project

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
+/* Compute interest */
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+
+    var interest = principal * years * rate / 100;
+
+    var year = new Date().getFullYear() + parseInt(years);
+
+    /* Output result as text in `result` element */
+    document.getElementById("result").innerHTML="If you deposit " +
+      principal + ",\<br\>at an interest rate of " + rate +
+      "%\<br\>You will receive an amount of " + interest +
+      ",\<br\>in the year " + year + "\<br\>"
 }
-        
+
+/* Display the rate as text next to the slider, `rate_val` element */
+function updateRate()
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval + " %";
+}
+
+/* Show alert if invalid input in `principal` */
+function validatePrincipal()
+{
+  var principal = document.getElementById("principal").value;
+
+  if (principal != '' & principal <= 0) {
+    alert("Enter a positive number")
+    document.getElementById("principal").focus();
+  }
+}
